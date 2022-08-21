0174ff39dd373e1c26e5559c378882fd7b677f7a
Brian Denmark <testingarea479@gmail.com>
2022-08-21 14:19:20 -0700

    completed level 2

--- server.py
+++ server.py
@@ -5,9 +5,9 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 def index():
     return render_template('index.html')  # Return the index file as a response
 
-@app.route('/play')          
-def play():
-    return render_template('play.html')  # 
+@app.route('/play/<int:x>')          
+def play(x):
+    return render_template('play.html', x = x)  # This will display the passed in amount of divs.
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
